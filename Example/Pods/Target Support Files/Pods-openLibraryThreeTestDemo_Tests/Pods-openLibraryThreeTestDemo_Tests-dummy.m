84fd05c6b368103084522bcc31169646f72449f6
liquangang <liquangang@idstaff.com>
2017-08-11 17:46:48 +0800

    初始化工程

---
+++ Example/Pods/Target Support Files/Pods-openLibraryThreeTestDemo_Tests/Pods-openLibraryThreeTestDemo_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_openLibraryThreeTestDemo_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_openLibraryThreeTestDemo_Tests
+@end
---
+++ Example/Pods/Target Support Files/openLibraryThreeTestDemo/openLibraryThreeTestDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_openLibraryThreeTestDemo : NSObject
+@end
+@implementation PodsDummy_openLibraryThreeTestDemo
+@end
